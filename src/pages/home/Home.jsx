7378ed37674a4f24bc09da3a2f1ef8fe507b82a6
Ivana <ivanajankovic1302@gmail.com>
2025-02-01 20:36:38 +0100

    key removed

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -15,7 +15,7 @@ const Home = () => {
 
     const darkMode=theme.state.darkMode;
 
-    const apiKey = "b73497ce3347473d9f12fb64cd0b4210";
+    const apiKey = ;
     const url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=${apiKey}`;
 
     const getNews = async () => {
